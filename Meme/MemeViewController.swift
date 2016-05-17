eb101af7ad67644788e1b37ae65c7e24dba07b13
alexscottzs <alexzhangshuo@gmail.com>
2016-05-17 22:43:37 +0800

    feat: Picking an image!

---
+++ Meme/MemeViewController.swift
@@ -0,0 +1,44 @@
+//
+//  MemeViewController.swift
+//  Meme
+//
+//  Created by HhhotDog on 16/5/17.
+//  Copyright © 2016年 Alexscott. All rights reserved.
+//
+
+import UIKit
+
+class MemeViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
+    }
+    
+    @IBAction func pickUpAnImageFromAlbum() {
+        pickUpAnImage(UIImagePickerControllerSourceType.PhotoLibrary)
+    }
+    
+    @IBAction func pickUpAnImageFromCamera() {
+        pickUpAnImage(UIImagePickerControllerSourceType.Camera)
+    }
+    
+    //MARK: Helper
+    private func pickUpAnImage(source:UIImagePickerControllerSourceType){
+        let imagePickerVC = UIImagePickerController()
+        imagePickerVC.delegate = self
+        imagePickerVC.sourceType = source
+        presentViewController(imagePickerVC, animated: true, completion: nil)
+    }
+    
+    //MARK:UIImagePickerControllerDelegate
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage{
+            imageView.image = image
+        }
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+}
+
